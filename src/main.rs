d77c0a3be27039f37b8a3aae8d44d1e5b51e0f7b
jakubslaby09 <jakubslaby09@gmail.com>
2024-09-19 19:45:22 +0200

    stahovat zvlášť sekce, použít get namísto post

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
-use std::{error::Error, fs::File, string::String};
+use std::path::PathBuf;
+use std::{fs::File, string::String};
 
 use anyhow::Result;
-use reqwest::blocking::Client;
+use reqwest::{blocking::Client, Url};
 use scraper::{selectable::Selectable, ElementRef, Html, Selector};
 use serde::Serialize;
 use std::io::Write;
 use std::cmp::Reverse;
 
-const URL: &str = "https://etesty2.mdcr.cz/Vestnik/ShowPartial";
+const SECTIONS_URL: &str = "https://etesty2.mdcr.cz/Vestnik";
+const SECTION_BASE_URL: &str = "https://etesty2.mdcr.cz";
+const ALL_URL: &str = "https://etesty2.mdcr.cz/Vestnik/ShowPartial";
 const OUTPUT_CSV: &str = "./scrape.csv";
 const OUTPUT_HTML: &str = "./scrape.html";
 const PAGE_LIMIT: usize = 200;
 
+const SECTIONS_SELECTOR: &str = "#VerticalMenuPanel > ul > li > a";
 const QUESTION_SELECTOR: &str = "body > div.QuestionPanel";
 const QUESTION_TEXT_SELECTOR: &str = "div.QuestionText";
 const QUESTION_CODE_SELECTOR: &str = "span.QuestionCode";
@@ -21,45 +25,89 @@ const QUESTION_ANSWER_TEXT_SELECTOR: &str = "div.AnswerText > a";
 const QUESTION_VIDEO_SELECTOR: &str = "div.QuestionImagePanel > video > source";
 const QUESTION_IMAGE_SELECTOR: &str = "div.QuestionImagePanel > img";
 
+fn out_html_path(section: &Section) -> PathBuf {
+    format!("./scrape.{}.{}.html", section.section_id, truncate(&section.name, 30)).into()
+}
+fn out_csv_path(section: &Section) -> PathBuf {
+    format!("./scrape.{}.{}.csv", section.section_id, truncate(&section.name, 30)).into()
+}
+
 // TODO: #![deny(clippy::unwrap_used)]
 
-fn main() {
+fn main() -> Result<()> {
     let client = Client::new();
+    
+    eprintln!("scraping full");
+    scrape_section(&client, None)?;
+    eprintln!(" listing sections");
+    for section in list_sections(&client)?.into_iter() {
+        eprintln!(" scraping {}", section.name);
+        scrape_section(&client, Some(section))?
+    };
+    eprintln!("done");
+    Ok(())
+}
 
-    let csv_file = File::create(OUTPUT_CSV).expect("couldn't make a file");
-    let mut html_file = File::create(OUTPUT_HTML).expect("couldn't make a file");
-    let mut writer = csv::Writer::from_writer(csv_file);
+fn list_sections(client: &Client) -> Result<Vec<Section>> {
+    let res = client.get(SECTIONS_URL).send()?.text()?;
+    let selector = Selector::parse(SECTIONS_SELECTOR).unwrap();
+    let html = Html::parse_document(&res);
+    Ok(html.select(&selector).map(|link| {
+        let href = link.attr("href").unwrap();
+        Section {
+            name: link.text().collect(),
+            section_id: Url::parse(&format!("{SECTION_BASE_URL}{href}")).unwrap()
+            .query_pairs().find(|q| q.0 == "basketScope").unwrap().1.to_string(),
+        }
+    }).collect())
+}
 
+fn scrape_section(client: &Client, section: Option<Section>) -> Result<()> {
+    let output_html_path = match &section {
+        Some(section) => out_html_path(&section),
+        None => OUTPUT_HTML.into(),
+    };
+    let output_csv_path = match &section {
+        Some(section) => out_csv_path(&section),
+        None => OUTPUT_CSV.into(),
+    };
+    let mut output_html = File::create(output_html_path).expect("couldn't make a file");
+    let csv_file = File::create(output_csv_path).expect("couldn't make a file");
+    let mut csv_writer = csv::Writer::from_writer(csv_file);
+    
     for page in 1..PAGE_LIMIT {
-        if let Some(res) = parse_page(&client, page, &mut html_file).unwrap() {
-            eprintln!("fetched and parsed page {page}");
+        if let Some(res) = scrape_page(&client, section.as_ref(), page, &mut output_html).unwrap() {
+            eprintln!("  fetched and parsed page {page}");
             for question in res {
-                writer.serialize(question).unwrap();
+                csv_writer.serialize(question).unwrap();
             }
         } else {
-            eprintln!("done scraping {} pages", page.saturating_sub(1));
+            eprintln!("  done scraping {} pages", page.saturating_sub(1));
             break;
         }
     }
 
-    eprintln!("writing to a file...");
-    // writeln!(result_file, "{}", ).expect("couldn't write results to a file");
-    eprintln!("done");
+    Ok(())
 }
 
-fn parse_page(client: &Client, page: usize, output_html: &mut File) -> Result<Option<Vec<Question>>> {
-    let res = client.post(URL)
-    .body(format!("page={page}"))
-    .header("content-type", "application/x-www-form-urlencoded")
-    .send()?;
+fn scrape_page(client: &Client, section: Option<&Section>, page: usize, output_html: &mut File) -> Result<Option<Vec<Question>>> {
+    // let url = Url::parse_with_params(match section {
+    //     Some(section) => section.url.as_str(),
+    //     None => ALL_URL,
+    // }, &[("page", &page.to_string())])?;
+    let url = match section {
+        Some(section) => Url::parse_with_params(ALL_URL, &[("page", &page.to_string()), ("basketScope", &section.section_id)]),
+        None => Url::parse_with_params(ALL_URL, &[("page", &page.to_string()), ]),
+    }?;
+    let res = client.get(url)
+    .send()?.text()?;
 
-    let text = res.text()?;
-    if text.trim().is_empty() {
+    if res.trim().is_empty() {
         return Ok(None);
     }
     
-    writeln!(output_html, "<!-- Page {page} -->\n{}", text).expect("couldn't write results to a file");
-    let document = Html::parse_document(&text);
+    writeln!(output_html, "<!-- Page {page} -->\n{}", res).expect("couldn't write results to a file");
+    let document = Html::parse_document(&res);
 
     let questions_selector = Selector::parse(QUESTION_SELECTOR).unwrap();
     let questions = document.select(&questions_selector);
@@ -103,6 +151,13 @@ fn parse_question(element: ElementRef) -> Result<Question, String> {
     })
 }
 
+fn truncate(name: &str, max: usize) -> String {
+    if name.chars().count() < max {
+        return name.to_string();
+    }
+    name.chars().take(max).collect()
+}
+
 #[derive(Debug, Serialize)]
 struct Question {
     code: String,
@@ -125,13 +180,7 @@ enum QuestionMedia {
 }
 
 #[derive(Debug)]
-struct NoPagesLeftError {
-    pub page: usize
+struct Section {
+    pub name: String,
+    pub section_id: String,
 }
-
-impl std::fmt::Display for NoPagesLeftError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "no pages left at page {}", self.page)
-    }
-}
-impl Error for NoPagesLeftError {}
